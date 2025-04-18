172d632fa0e78c449eb6ccb0e41cc6c31a2ac304
Rishabh <rsharma4_be23@thapar.edu>
2025-04-19 02:49:09 +0530

    gave modern look using tailwind

--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,7 @@
-import { useEffect ,useState} from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import "tailwindcss";
 
+import "tailwindcss";
 const API_URL="http://localhost:5000/messages"
 function App(){
   const [username,setUsername]=useState("");
@@ -35,45 +35,45 @@ function App(){
     }
   }
   return(
-    <div className="min-h-screen bg-gradient-to-r from-violet-500 to-fuchsia-400 flex items-center justify-center">
+    <div className="min-h-screen bg-[#0e0f1a] text-white font-['Inter'] flex items-center justify-center p-5" >
     {!username ? (
-      <div className="p-8 bg-white rounded-lg shadow-md max-w-md w-full">
-        <h2 className="text-2xl font-bold mb-4 text-center">Enter your Name to Join Chat</h2>
-        <div className="flex flex-col sm:flex-row items-center">
-          <input 
-            className="border border-gray-300 rounded px-4 py-2 w-full sm:w-auto mb-3 sm:mb-0 sm:mr-2"
+      <div className="text-center space-y-4 bg-[#1a1b2e] p-8 rounded-2xl shadow-xl">
+        <h2 className="text-2xl font-semibold mb-8" >Enter your Name to Join Chat</h2>
+        <div className="flex flex-col space-y-2">
+          <input
             value={tempUsername} 
             onChange={(e)=>setTempUsername(e.target.value)}
             onKeyDown={(e)=>{
               if(e.key == "Enter") setUsername(tempUsername);
             }}
+            placeholder="Your Name "
+            className="px-4 py-2 rounded-md bg-[#2e2f3f] text-white w-full focus:outline-none"
           />
-          <button 
-            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 w-full sm:w-auto"
-            onClick={()=> setUsername(tempUsername)}>
+          <button className=" bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-md transition"onClick={()=> setUsername(tempUsername)}>
             Join Chat
           </button>
         </div>
       </div>
     ):(
-      <div className="p-5 bg-white rounded-lg shadow-md max-w-2xl w-full">
-        <h2 className="text-xl font-bold mb-3">Hello, {username}</h2>
-        <div className="border border-gray-300 h-[300px] overflow-y-scroll p-2.5 mb-2.5">
+      <div className="w-full max-w-2xl bg-[#1a1b2e] rounded-2xl shadow-xl p-6 mt-4"  >
+        <h2 className="text-2xl font-semibold text-center">Hello, {username}</h2>
+        <h2 className=" text-lg font-medium">Chats</h2>
+        <div className="space-y-2 max-h-[400px] overflow-y-auto pr-2 ">
           {chat.map((msg,i)=>(
-            <div key={i} className="mb-1">
-              <strong className="font-semibold">{msg.user}:</strong>{msg.message}
+            <div key={i} className="bg-[#232435] px-3 py-2 rounded-md">
+              <span className="font-semibold text-blue-400">{msg.user}:  <span className="text-gray-200">{msg.message}</span></span>
             </div>
           ))}
         </div>
-        <div className="flex">
+        <div className="flex gap-2">
           <input
-            className="border border-gray-300 rounded px-2 py-1 flex-grow mr-2"
             value={message}
             onChange={(e)=> setMessage(e.target.value)}
             onKeyDown={handleEnter}
             placeholder="Type your message..." 
+            className="flex-1 px-4 py-2 rounded-md bg-[#2e2f3f] text-white focus:outline-none"
           />
-          <button className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600" onClick={sendMessage}>Send</button>
+          <button onClick={sendMessage} className="bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-r-lg px-4 py-2 transition-colors duration-300 focus:outline-none focus:ring-none focus:ring-blue-500 shadow-md">Send</button>
         </div>
       </div>
     )}
